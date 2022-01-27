db4e11a8593ace04066e2a6381492a9dd358d568
Valery Efremov <valeriy131100@gmail.com>
2022-01-27 08:55:04 +0300

    Connect static

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -94,7 +94,10 @@ USE_TZ = True
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+)
 
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
 
